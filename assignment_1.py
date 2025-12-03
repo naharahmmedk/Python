96e3940566ebdd38d7401c90beb0066af601463c
Nahar Ahammed <57092595+naharahmmedk@users.noreply.github.com>
2025-12-03 23:15:08 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,14 @@
+# Numeric operations
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# String concatenation
+first = "Welcome To"
+second = "Python"
+print(first + " " + second)
+
+
